79cde6b11d57feb1beffda9d672dc1a5c47e43ec
Thiago Henrique Poiani <thpoiani@gmail.com>
2015-04-19 08:27:26 +0000

    i18n

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?php echo 'Hello World'; ?>
+<?php
+
+require("vendor/autoload.php");
+
+use Symfony\Component\Translation\Translator;
+use Symfony\Component\Translation\Loader\ArrayLoader;
+
+$translator = new Translator(null);
+
+$translator->addLoader('array', new ArrayLoader());
+
+$translator->addResource('array',
+    array('Hello World' => 'Olá mundo'), 'pt'
+);
+
+$translator->addResource('array',
+    array('Hello World' => 'Hola Mundo'), 'es'
+);
+
+$language = isset($argv[1]) ? $argv[1] : null;
+$translator->setLocale($language);
+
+echo $translator->trans('Hello World');
